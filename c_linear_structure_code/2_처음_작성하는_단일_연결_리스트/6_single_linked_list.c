59f21630e4571c33a0a0eb546653536da6a63342
feelchok1234@gmail.com <feelchok1234@gmail.com>
2026-09-26 02:02:08 +0900

    add, search clear, delete, test

--- c_linear_structure_code/2_처음_작성하는_단일_연결_리스트/6_single_linked_list.c
+++ c_linear_structure_code/2_처음_작성하는_단일_연결_리스트/6_single_linked_list.c
@@ -53,6 +53,8 @@ void print_all_node () {
         print_head_node = print_head_node->p_next;
    }
 
+   printf("\n");
+
 }
 
 void init () {
@@ -74,12 +76,72 @@ void clear_node () {
     global_head_node = NULL;
 }
 
+void search_address_print (char * target) {
+
+    printf("target : %s\n", target);
+
+    USERDATA * search_head_node = global_head_node;
+
+    int boolean = 0;
+
+    while (search_head_node != NULL) {
+        if (strcmp(target, search_head_node->name) == 0) {
+            boolean = 1;
+            printf("Found!\n [Current Address : %p] name : %s, age : %d, phone : %s, [Next Address : %p]",
+                search_head_node, search_head_node->name, search_head_node->age, search_head_node->phone, search_head_node->p_next
+            );
+        } 
+        search_head_node = search_head_node->p_next;
+    }
+
+    if (!boolean) {
+        printf("Not Found!\n");
+    }
+
+    printf("\n");
+
+}
+
+void delete_node(char *target)
+{
+    USERDATA *current = global_head_node;
+    USERDATA *prev = NULL;
+
+    while (current != NULL) {
+
+        if (strcmp(current->name, target) == 0) {
+
+            // 첫 번째 노드인 경우
+            if (prev == NULL) {
+                global_head_node = current->p_next;
+            }
+
+            // 두 번째 이후 노드인 경우
+            else {
+                prev->p_next = current->p_next;
+            }
+
+            free(current);
+            return;
+        }
+
+        prev = current;
+        current = current->p_next;
+    }
+
+    printf("삭제할 노드를 찾지 못했습니다.\n");
+}
 
 int main (void) {
 
     init();
     print_all_node();
-    clear_node();
+    search_address_print("Hong");
+    search_address_print("KKK");
+    delete_node("Hong");
+    delete_node("Hong");
+    delete_node("H");
+
 
     return 0;
 }
